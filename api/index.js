bf0bab074093e4a2ce35a9de102d9451aa0e2900
Matt Fan <mfanstudent@gmail.com>
2020-03-17 17:35:59 -0400

    add skeleton for inviteFriend endpoint wrapper

--- api/index.js
+++ api/index.js
@@ -3,3 +3,5 @@ import * as utils from './utils'
 export const getUserInfo = () => utils.make_skeleton_endpoint()
 
 export const getExposureRisk = () => utils.make_skeleton_endpoint({ exposureRisk: 'Normal' })
+
+export const inviteFriend = (phone_number) => utils.make_skeleton_endpoint()
