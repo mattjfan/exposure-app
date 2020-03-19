940adbc84cdcc66977476a8b0f61d329437a1d25
Matt Fan <mfanstudent@gmail.com>
2020-03-19 01:40:22 -0400

    add basic storage api, UNTESTED

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import * as utils from './utils'
+import * as location from './location'
+import * as storage from './storage'
 
 export const getUserInfo = () => utils.make_skeleton_endpoint()
 
 export const getExposureRisk = () => utils.make_skeleton_endpoint({ exposureRisk: 'Normal' })
 
 export const inviteFriend = (phone_number) => utils.make_skeleton_endpoint()
+
+export { location, storage }
---
+++ api/storage.js
@@ -0,0 +1,35 @@
+import { AsyncStorage } from 'react-native';
+
+const CONTACTED_TOKENS_KEY = '@EXPOSURE_APP_CONTACTED_TOKENS' // object of form { token: last_contact_timestamp} for all recorded contacts
+const MY_TOKENS_KEY = '@EXPOSURE_APP_SELF_TOKENS' // list of all known tokens given to others as a self identifier... Currently unused
+
+export const putContactedTokens = (tokens) => {
+    AsyncStorage.getItem(
+        CONTACTED_TOKENS_KEY,
+        (err, _old_tokens) => {
+            const old_tokens = _old_tokens ?  JSON.parse(_old_tokens) : {}
+            tokens.forEach(tkn => {
+                old_tokens[tkn] =  new Date() // Just keeping most recent contact
+            });
+            AsyncStorage.setItem(CONTACTED_TOKENS_KEY, JSON.stringify(old_tokens))
+        }
+    )
+}
+export const getContactedTokensAfter = async (date) => {
+    const user_tokens = await AsyncStorage.getItem(CONTACTED_TOKENS_KEY).then(res => res ? JSON.parse(res) : {})
+    return user_tokens ? (
+            Object.keys(user_tokens)
+            .filter(k => new Date(user_tokens[k]) > date) // filter for last contact days AFTER specified date
+        )
+        : [] // return empty array by default
+}
+
+export const sendUserTokensAfter = date => null //UNIMPLEMENTED
+
+export const getUserTokens = () => {
+    return getUserTokensAfter(new Date(0))
+}
+
+export const updateNearbyUsers = (tokens) => {
+
+}
